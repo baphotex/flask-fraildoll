a71f72371190e2a24743a78600dd636010693c97
ruby <136928692+baphotex@users.noreply.github.com>
2024-01-11 02:02:20 -0600

    Update refeedingCalculator.js

--- static/js/refeedingCalculator.js
+++ static/js/refeedingCalculator.js
@@ -66,7 +66,7 @@ function calculate() {
     var row2 = table.insertRow();
     row2.insertCell(0).innerHTML = "<b>Calories</b>";
     for (var day = 1; day <= 7; day++) {
-        row2.insertCell(day).innerHTML = day * 5 * weight;
+        row2.insertCell(day).innerHTML = Math.round(day * 5 * weight);
     }
 
     // Append the table to the resultTable div
